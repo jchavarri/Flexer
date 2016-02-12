01378e19caa021b5ffe17369b02cea3925195679
Javier Chávarri <javier.chavarri@gmail.com>
2016-02-12 05:09:13 +0100

    Changing title

--- framer/framer.generated.js
+++ framer/framer.generated.js
@@ -7,7 +7,7 @@ window.Framer.Defaults.DeviceView = {"deviceScale":-1,"deviceType":"iphone-6-sil
 
 window.Framer.Defaults.DeviceComponent = {"deviceScale":-1,"deviceType":"iphone-6-silver","contentScale":1,"orientation":0};
 
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Untitled.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Flexer example"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
